54ddd52bf53fc64d9fa98ecd47d4e1ee53be878c
RiceyuCui <646017778@qq.com>
2017-08-13 20:59:57 +0800

    standardio

--- standardio.c
+++ standardio.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
 
 #define FILENAME "1.txt"
 int main(void)
 {
 	FILE *fp = NULL;
-	fp  = fopen(FILENAME, "w+");
+	size_t len = -1;
+	int array[10] = {1, 2, 3, 4, 5};
+	char buf[100] = {};
+	fp  = fopen(FILENAME, "r+");
 
 	if (NULL == fp)
 	{
@@ -14,6 +19,11 @@ int main(void)
 	}
 
 	printf("fopen success. fp = %p.\n", fp);
+	memset(buf, 0, sizeof(buf));
+	len = fread(buf, 1, 10, fp);
+	printf("len = %d.\n", len);
+
+	printf("bus is : [%s].\n", buf);
 
 	fclose(fp);
 
