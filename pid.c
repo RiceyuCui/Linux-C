958e1eb93793ed4456685fcb2962c88dc7d17002
RiceyuCui <646017778@qq.com>
2017-08-15 10:33:34 +0800

    parent and child process commit

--- pid.c
+++ pid.c
@@ -7,23 +7,23 @@ int main (void)
 {
 	int fd = -1;
 	int pid = -1;
-	fd = open("1.txt", O_RDWR | O_TRUNC);
 
 	if (fd < 0)
 	{
 		perror("open");
-		return -1;
 
 	}
 	pid = fork();
 	if (pid > 0)
 	{
+		fd = open("1.txt", O_RDWR);
 		printf("parent process.\n");
 		write(fd, "hello", 5);
 		sleep(1);
 	}
 	else if (pid == 0)
 	{
+		fd = open("1.txt", O_RDWR);
 		printf("child process.\n");
 		write(fd, "world", 5);
 		sleep(1);
